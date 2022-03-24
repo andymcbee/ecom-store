50b1bc544e3b9e5dd03a3d0162bddd042475cb2a
Andrew <armcburn@gmail.com>
2022-03-24 15:48:53 -0400

    Add routes home and product

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomePage from "./Routes/HomePage.js";
+import ProductPage from "./Routes/ProductPage.js";
 
 export default function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Store</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Store</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductPage />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
