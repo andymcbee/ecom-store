13b4e13132b9510772f2602bbd5adda33107aebc
Andrew <armcburn@gmail.com>
2022-03-24 13:13:33 -0400

    Add header and data

---
+++ client/src/data.js
@@ -0,0 +1,55 @@
+const data = {
+  products: [
+    {
+      name: "Nike Slim shirt",
+      slug: "nike-slim-shirt",
+      category: "shirts",
+      image: "/images/p1.jpg",
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "High quality shirt!",
+    },
+
+    {
+      name: "Adidas Shirt",
+      slug: "adidas-shirt",
+      category: "shirt",
+      image: "/images/p2.jpg",
+      price: 45,
+      countInStock: 3,
+      brand: "Adidas",
+      rating: 3.2,
+      numReviews: 3,
+      description: "A great shirt!",
+    },
+
+    {
+      name: "Adidas Fit Pants",
+      slug: "adidas-fit-pants",
+      category: "pants",
+      image: "/images/p3.jpg",
+      price: 64,
+      countInStock: 12,
+      brand: "Adidas",
+      rating: 4.8,
+      numReviews: 22,
+      description: "High quality pants that fit well!",
+    },
+
+    {
+      name: "Nike Slim pants",
+      slug: "nike-slim-pants",
+      category: "pants",
+      image: "/images/p4.jpg",
+      price: 25,
+      countInStock: 7,
+      brand: "Nike",
+      rating: 4.2,
+      numReviews: 3,
+      description: "pants!",
+    },
+  ],
+};
